66621ac326a0f2c3147ab53324ba05fda80a3f20
ksmith2012 <ksmith@uxstrategy.ca>
2018-10-22 20:45:25 -0400

    Activity 10 - updated

--- assets/ik_tabs.js
+++ assets/ik_tabs.js
@@ -3,7 +3,7 @@
 	var pluginName = 'ik_tabs',
 		defaults = {
 			tabLocation: 'top',
-			selectedIndex: 0
+			selectedIndex: 1
 		};
 
 	/**
@@ -114,7 +114,8 @@
 				tabindex: 0
 			});
 
-		if (event.type) $($tabs[ind]).focus(); // move focus to current tab if reached by mouse or keyboard
+		// if (event.type)
+		$($tabs[ind]).focus(); // move focus to current tab if reached by mouse or keyboard
 
 		$panels // hide all panels
 			.attr({
@@ -148,27 +149,45 @@
 	    $tabs = plugin.tabs;
 	    $panels = plugin.panels;
 
-	    switch (event.keyCode) {
-	        case ik_utils.keys.left:
-	        case ik_utils.keys.up:
-							next = (ind > 0) ? ind - 1 : 0;
-	            // plugin.selectTab({data:{'plugin': plugin, 'index': next}});
-							event.data.index = next;
-							plugin.selectTab(event);
-	            break;
-	        case ik_utils.keys.right:
-	        case ik_utils.keys.down:
-	            next = (ind < $tabs.length - 1) ? ind + 1 : $tabs.length - 1;
-							/// plugin.selectTab({data:{'plugin': plugin, 'index': next}});
-							event.data.index = next;
-							plugin.selectTab(event);
+	    // switch (event.keyCode) {
+	    //     case ik_utils.keys.left:
+	    //     case ik_utils.keys.up:
+			// 				next = (ind > 0) ? ind - 1 : 0;
+	    //         // plugin.selectTab({data:{'plugin': plugin, 'index': next}});
+			// 				event.data.index = next;
+			// 				plugin.selectTab(event);
+	    //         break;
+	    //     case ik_utils.keys.right:
+	    //     case ik_utils.keys.down:
+	    //         next = (ind < $tabs.length - 1) ? ind + 1 : $tabs.length - 1;
+			// 				/// plugin.selectTab({data:{'plugin': plugin, 'index': next}});
+			// 				event.data.index = next;
+			// 				plugin.selectTab(event);
+			//
+	    //         break;
+	    //     case ik_utils.keys.space:
+	    //         event.preventDefault();
+	    //         event.stopPropagation();
+	    //         return false;
+	    // }
 
-	            break;
-	        case ik_utils.keys.space:
-	            event.preventDefault();
-	            event.stopPropagation();
-	            return false;
-	    }
+			switch (event.keyCode) {
+			        case ik_utils.keys.left:
+			        case ik_utils.keys.up:
+			            next = ind > 0 ? --ind : 0;
+			            plugin.selectTab({data:{'plugin': plugin, 'index': next}});
+			            break;
+			        case ik_utils.keys.right:
+			        case ik_utils.keys.down:
+			            next = ind < $tabs.length - 1 ? ++ind : $tabs.length - 1;
+			            plugin.selectTab({data:{'plugin': plugin, 'index': next}});
+			            break;
+			        case ik_utils.keys.space:
+			            event.preventDefault();
+			            event.stopPropagation();
+			            return false;
+			    }
+
 	}
 
 
